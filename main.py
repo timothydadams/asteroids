b0929e2a2913b2acc65eae03145b386e6f8d467a
Timothy Adams <adams.timothy.d@gmail.com>
2025-04-18 14:00:22 -0400

    full import

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from pygame import event as e, display, Surface, QUIT
+import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_KINDS, ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS,ASTEROID_SPAWN_RATE
 
 def main():
-    # pygame.init()
+    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
     try:
         while True:
-            for event in e.get():
-                if event.type == QUIT:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
                     return
                 
-            Surface.fill(screen, (0,0,0))
-            display.flip()
+            screen.fill("black")
+            pygame.display.flip()
     except KeyboardInterrupt:
         print("Exited loop form keyboard interrupt")
     finally:
