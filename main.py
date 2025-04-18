cc051c904827506b3b6513579aab0ac4829c7039
Timothy Adams <adams.timothy.d@gmail.com>
2025-04-18 14:27:14 -0400

    add circle class

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_KINDS, ASTEROID_MAX_
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT], display=2)
 
     try:
         while True:
@@ -16,6 +18,7 @@ def main():
                 
             screen.fill("black")
             pygame.display.flip()
+            dt = clock.tick(60) / 1000
     except KeyboardInterrupt:
         print("Exited loop form keyboard interrupt")
     finally:
